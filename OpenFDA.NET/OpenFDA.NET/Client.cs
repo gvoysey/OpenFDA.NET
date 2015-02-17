a9d8c4a8312ae59e2de56c68d00dc21357874c52
Graham Voysey <gvoysey@bu.edu>
2015-02-17 14:28:40 -0500

    integrating NEST

---
+++ OpenFDA.NET/OpenFDA.NET/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace OpenFDA.NET
+{
+    public class Client
+    {
+        public static string Get()
+        {
+            var url = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:\"nonsteroidal+anti-inflammatory+drug\"&count=patient.reaction.reactionmeddrapt.exact";
+
+            var syncClient = new WebClient();
+
+            return syncClient.DownloadString(url);
+        }
+
+        public static void GetNEST()
+        {
+            var node = new Uri("https://api.fda.gov/");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //client.Search( new SearchRequest{})
+        }
+    }
+}
